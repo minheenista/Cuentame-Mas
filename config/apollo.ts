bc2b6d5766af28b8052048d00b286f0add2bd986
Mich <anamichelle1308@gmail.com>
2024-09-24 13:14:28 -0600

    :wrench: Update Env

--- config/apollo.ts
+++ config/apollo.ts
@@ -1,12 +1,12 @@
 import { ApolloClient } from "@apollo/client";
 import { InMemoryCache } from "@apollo/client";
 import { HttpLink } from "@apollo/client";
-/* import { ENDPOINT_URL } from ".env";
- */
+import { URI } from "./../constants/constants";
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
-    uri: "https://cuentame-mas-api.onrender.com/graphql",
+    uri: URI,
   }),
 });
 
---
+++ constants/constants.ts
@@ -0,0 +1,1 @@
+export const URI = process.env.ENDPOINT_URL;
