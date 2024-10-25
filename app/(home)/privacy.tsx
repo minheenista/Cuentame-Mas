08ec0fe344e44b637cf2c6aec5ffa5d6e825e1b7
Mich <anamichelle1308@gmail.com>
2024-10-25 16:10:31 -0600

    👍 Tos & Privacy Ui

--- app/(home)/privacy.tsx
+++ app/(home)/privacy.tsx
@@ -5,52 +5,157 @@ import {
   Text,
   View,
   Pressable,
+  Dimensions,
+  useColorScheme,
+  ScrollView,
 } from "react-native";
-
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
-import { router } from "expo-router";
+import { Colors } from "@/constants/Colors";
+import NavBar from "@/components/NavBar";
 
 export default function PrivacyScreen() {
+  const width = Dimensions.get("window").width;
+
+  const colorScheme = useColorScheme();
+  const isDarkMode = colorScheme === "dark";
+  const activeColors = Colors[isDarkMode ? "dark" : "light"];
+
   return (
-    <View>
-      <Text>PRIVACY SCREEN</Text>
-      <Pressable onPress={() => router.navigate("/")}>
-        <Text>HOME</Text>
-      </Pressable>
-      <Pressable onPress={() => router.navigate("/faq")}>
-        <Text>FAQ</Text>
-      </Pressable>
-      <Pressable onPress={() => router.navigate("/tos")}>
-        <Text>TOS</Text>
-      </Pressable>
-      <Pressable onPress={() => router.navigate("/privacy")}>
-        <Text>PRIVACY</Text>
-      </Pressable>
-      <Pressable onPress={() => router.navigate("/login")}>
-        <Text>LOGIN</Text>
-      </Pressable>
-    </View>
+    <ScrollView style={{ backgroundColor: activeColors.background }}>
+      {width > 880 && (
+        <View>
+          <NavBar></NavBar>
+        </View>
+      )}
+      <View style={styles(activeColors).container}>
+        <Text
+          style={
+            width > 720
+              ? styles(activeColors).titleWeb
+              : styles(activeColors).titleMobile
+          }
+        >
+          Políticas de Privacidad
+        </Text>
+        <Text style={[styles(activeColors).h6, { marginBottom: 20 }]}>
+          Última actualización: 11 de febrero de 2024
+        </Text>
+        <Text style={styles(activeColors).text}>
+          Este Aviso de Privacidad fue redactado conforme los principios de
+          licitud, consentimiento, información, calidad, finalidad, lealtad,
+          proporcionalidad y responsabilidad establecidos en la Ley Federal de
+          Protección de Datos Personales en Posesión de Particulares (LFPDPPP)
+          publicada en el Diario Oficial de la Federación el 05-07-2010 y las
+          Leyes aplicables dentro de la jurisdicción correspondiente.
+        </Text>
+        <Text
+          style={[
+            styles(activeColors).h5,
+            { alignSelf: "center", marginVertical: 20 },
+          ]}
+        >
+          RESUMEN DE LA POLITICA DE PRIVACIDAD
+        </Text>
+
+        <Text style={[styles(activeColors).h5]}>
+          Datos Personales tratados para las siguientes finalidades y utilizando
+          los siguientes servicios:
+        </Text>
+
+        <Text style={[styles(activeColors).text, { marginLeft: 10 }]}>
+          - Contactar con el Usuario
+        </Text>
+
+        <Text style={[styles(activeColors).text, { marginLeft: 30 }]}>
+          - Formulario de contacto
+        </Text>
+        <Text style={[styles(activeColors).text, { marginLeft: 30 }]}>
+          Datos Personales: nombre; apellido(s); dirección de correo
+          electrónico.
+        </Text>
+
+        <Text style={[styles(activeColors).text, { marginLeft: 10 }]}>
+          - Permisos sobre dispositivos para acceder a Datos Personales
+        </Text>
+
+        <Text style={[styles(activeColors).text, { marginLeft: 30 }]}>
+          - Permisos sobre dispositivos para acceder a Datos Personales
+        </Text>
+        <Text style={[styles(activeColors).text, { marginLeft: 30 }]}>
+          Datos Personales: Permiso de ubicación aproximada (en modo no
+          continuo); Permiso para acceder a la ubicación exacta (en modo no
+          continuo);
+        </Text>
+        <Text style={[styles(activeColors).text, { marginLeft: 30 }]}>
+          Permiso para la cámara; Permiso para la Galería de Fotos
+        </Text>
+
+        <Text style={[styles(activeColors).text, { marginLeft: 10 }]}>
+          - Registro y autenticación
+        </Text>
+
+        <Text style={[styles(activeColors).text, { marginLeft: 30 }]}>
+          - Firebase Authentication
+        </Text>
+
+        <Text style={[styles(activeColors).text, { marginLeft: 30 }]}>
+          Datos Personales: nombre; apellido(s); username; cuentas en redes
+          sociales; foto de perfil; dirección de correo electrónico.
+        </Text>
+        <Text style={[styles(activeColors).text, { marginLeft: 30 }]}>
+          - Facebook Oauth
+        </Text>
+        <Text style={[styles(activeColors).text, { marginLeft: 30 }]}>
+          Datos Personales: distintas clases de Datos; Rastreador
+        </Text>
+        <Text style={[styles(activeColors).text, { marginLeft: 30 }]}>
+          - Twitter OAuth y Google OAuth
+        </Text>
+
+        <Text style={[styles(activeColors).text, { marginLeft: 30 }]}>
+          Datos Personales: distintas clases de Datos, según se especifica en la
+          Política de Privacidad del servicio
+        </Text>
+      </View>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
-  },
-});
+const styles = (activeColors: any) =>
+  StyleSheet.create({
+    container: {
+      margin: 20,
+    },
+    titleWeb: {
+      fontSize: 60,
+      fontFamily: "Poppins-Regular",
+      color: activeColors.text,
+      textAlign: "center",
+      marginBottom: 20,
+    },
+    titleMobile: {
+      marginTop: 20,
+      fontSize: 24,
+      fontFamily: "Poppins-Regular",
+      color: activeColors.text,
+      textAlign: "center",
+      marginBottom: 20,
+    },
+    h6: {
+      fontFamily: "Poppins-Bold",
+      fontSize: 16,
+      color: activeColors.text,
+      letterSpacing: 0.15,
+    },
+    h5: {
+      fontFamily: "Poppins-Bold",
+      fontSize: 20,
+      color: activeColors.text,
+      letterSpacing: 0.15,
+    },
+    text: {
+      fontFamily: "Poppins-Regular",
+      fontSize: 16,
+      lineHeight: 24,
+      color: activeColors.text,
+    },
+  });
--- app/(home)/tos.tsx
+++ app/(home)/tos.tsx
@@ -1,56 +1,163 @@
 import {
-  Image,
   StyleSheet,
-  Platform,
   Text,
   View,
-  Pressable,
+  Dimensions,
+  useColorScheme,
+  ScrollView,
 } from "react-native";
-
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
-import { router } from "expo-router";
+import { Colors } from "@/constants/Colors";
+import NavBar from "@/components/NavBar";
 
 export default function TosScreen() {
+  const width = Dimensions.get("window").width;
+
+  const colorScheme = useColorScheme();
+  const isDarkMode = colorScheme === "dark";
+  const activeColors = Colors[isDarkMode ? "dark" : "light"];
+
   return (
-    <View>
-      <Text>TOS SCREEN</Text>
-      <Pressable onPress={() => router.navigate("/")}>
-        <Text>HOME</Text>
-      </Pressable>
-      <Pressable onPress={() => router.navigate("/faq")}>
-        <Text>FAQ</Text>
-      </Pressable>
-      <Pressable onPress={() => router.navigate("/tos")}>
-        <Text>TOS</Text>
-      </Pressable>
-      <Pressable onPress={() => router.navigate("/privacy")}>
-        <Text>PRIVACY</Text>
-      </Pressable>
-      <Pressable onPress={() => router.navigate("/login")}>
-        <Text>LOGIN</Text>
-      </Pressable>
-    </View>
+    <ScrollView style={{ backgroundColor: activeColors.background }}>
+      {width > 880 && (
+        <View>
+          <NavBar></NavBar>
+        </View>
+      )}
+      <View style={styles(activeColors).container}>
+        <Text
+          style={
+            width > 720
+              ? styles(activeColors).titleWeb
+              : styles(activeColors).titleMobile
+          }
+        >
+          Términos y Condiciones de Servicio
+        </Text>
+        <Text style={styles(activeColors).h6}>
+          Última actualización: 11 de febrero de 2024
+        </Text>
+        <Text
+          style={[
+            styles(activeColors).h5,
+            { alignSelf: "center", marginVertical: 20 },
+          ]}
+        >
+          ¡Te damos la bienvenida a Cuéntame +!
+        </Text>
+        <Text style={styles(activeColors).text}>
+          Las presentes Condiciones regulan:
+        </Text>
+        <Text style={[styles(activeColors).text, { marginLeft: 10 }]}>
+          - El uso de esta Aplicación,{" "}
+        </Text>
+        <Text
+          style={[
+            styles(activeColors).text,
+            { marginLeft: 10, marginBottom: 20 },
+          ]}
+        >
+          - y Cualquier otro Contrato o relación jurídica conexos celebrados con
+          el Titular de forma jurídicamente vinculante. Las palabras en
+          mayúsculas se definen en la sección correspondiente específica del
+          presente documento.
+        </Text>
+        <Text style={styles(activeColors).text}>
+          Los Usuarios deben leer atentamente el presente documento.
+        </Text>
+        <Text style={[styles(activeColors).text, { marginBottom: 20 }]}>
+          Ninguna de las disposiciones de las presentes Condiciones crea una
+          relación laboral, de agencia o de asociación entre las partes
+          involucradas. Esta Aplicación es ofrecida por: MICHISOFT S DE RL DE CV
+          UBICADA EN CALLE CIRCUITO DEL GATO 202, CIUDAD ADMINISTRATIVA, 98160
+          ZACATECAS, ZAC. Correo electrónico de contacto del Titular:
+          contacto.michisoft@gmail.com
+        </Text>
+        <Text style={[styles(activeColors).h5, { marginBottom: 20 }]}>
+          Información sobre esta Aplicación
+        </Text>
+        <Text style={styles(activeColors).text}>
+          Cuéntame+ es tu mejor compañero para resolver tus dudas de educación
+          financiera y contabilidad. En un mundo donde las decisiones
+          financieras son cada vez más complejas y determinantes para el
+          bienestar económico, Cuéntame+ ofrece una solución accesible y
+          personalizada.
+        </Text>
+        <Text style={styles(activeColors).text}>
+          «Esta Aplicación» hace referencia a:
+        </Text>
+        <Text style={[styles(activeColors).text, { marginLeft: 10 }]}>
+          - Esta página web, incluyendo sus subdominios y cualquier otra página
+          web mediante la cual el Titular facilite su Servicio;
+        </Text>
+        <Text style={[styles(activeColors).text, { marginLeft: 10 }]}>
+          - Aplicaciones para móviles, tabletas y otros sistemas de dispositivos
+          inteligentes;
+        </Text>
+        <Text
+          style={[
+            styles(activeColors).text,
+            { marginLeft: 10, marginBottom: 20 },
+          ]}
+        >
+          - El Servicio;
+        </Text>
+        <Text style={[styles(activeColors).h5, { marginBottom: 20 }]}>
+          Lo que el usuario debería saber de un vistazo
+        </Text>
+        <Text style={[styles(activeColors).text, { marginLeft: 10 }]}>
+          - La utilización de esta Aplicación y del Servicio está restringida en
+          función de la edad: para acceder a esta Aplicación y a sus Servicios y
+          utilizarlos es preciso ser mayor de edad según la ley aplicable.
+        </Text>
+        <Text
+          style={[
+            styles(activeColors).text,
+            { marginLeft: 10, marginBottom: 20 },
+          ]}
+        >
+          - El Servicio/esta Aplicación solo está destinado a los Consumidores.
+        </Text>
+      </View>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
-  },
-});
+const styles = (activeColors: any) =>
+  StyleSheet.create({
+    container: {
+      margin: 20,
+    },
+    titleWeb: {
+      fontSize: 60,
+      fontFamily: "Poppins-Regular",
+      color: activeColors.text,
+      textAlign: "center",
+      marginBottom: 20,
+    },
+    titleMobile: {
+      marginTop: 20,
+      fontSize: 24,
+      fontFamily: "Poppins-Regular",
+      color: activeColors.text,
+      textAlign: "center",
+      marginBottom: 20,
+    },
+    h6: {
+      fontFamily: "Poppins-Bold",
+      fontSize: 16,
+      color: activeColors.text,
+      letterSpacing: 0.15,
+    },
+    h5: {
+      fontFamily: "Poppins-Bold",
+      fontSize: 20,
+      letterSpacing: 0.15,
+      color: activeColors.text,
+    },
+    text: {
+      fontFamily: "Poppins-Regular",
+      fontSize: 16,
+      lineHeight: 24,
+      color: activeColors.text,
+    },
+  });
